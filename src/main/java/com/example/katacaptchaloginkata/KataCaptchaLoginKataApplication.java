613d4577882821686d06eff0a4c1312c5385c236
progden <ccdrumer@gmail.com>
2024-04-23 10:14:45 +0800

    新增 Gradle 配置和 Spring Boot 應用程式檔案

---
+++ src/main/java/com/example/katacaptchaloginkata/KataCaptchaLoginKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.katacaptchaloginkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataCaptchaLoginKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataCaptchaLoginKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/katacaptchaloginkata/KataCaptchaLoginKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katacaptchaloginkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataCaptchaLoginKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
