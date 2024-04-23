56ea9499e3336282d001e4a830eb6b526ed3511d
progden <ccdrumer@gmail.com>
2024-04-23 11:04:33 +0800

    新增參數化測試並改善登入驗證功能的模擬方法
    在KataCaptchaLoingTests.java中，我們新增了參數化測試，這可以用來測試不同的用戶名和密碼組合。另外，也改進了我們模擬登入驗證功能的方法，更貼近真實情況：驗證成功時返回 true。這些更動將提高我們測試的準確性和全面性。

--- src/test/java/com/example/katacaptchaloginkata/KataCaptchaLoingTests.java
+++ src/test/java/com/example/katacaptchaloginkata/KataCaptchaLoingTests.java
@@ -3,13 +3,14 @@ package com.example.katacaptchaloginkata;
 import com.example.kata.captchalogin.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @MockitoSettings(strictness = org.mockito.quality.Strictness.LENIENT)
@@ -27,7 +28,7 @@ class KataCaptchaLoingTests {
     @Test
     void login_when_correct_then_redirect_to_home() {
         givenStoredUserAndCaptcha("user", "captcha");
-        givenValidateUserSuccess(true);
+        givenCorrectValidateLogin("user", "password");
 
         login("user", "password", "captcha");
 
@@ -35,19 +36,34 @@ class KataCaptchaLoingTests {
         shouldRedirectTo("/home");
     }
 
-    @Test
-    void login_when_validate_user_fail_then_redirect_to_login() {
+    @ParameterizedTest(name = "login with wrong login user:{0}, pass:{1}")
+    @CsvSource({
+            "user, wrong-password",
+            "wrong-user, password"
+    })
+    void login_when_wrong_login_then_redirect_to_login(String user, String pass) {
         givenStoredUserAndCaptcha("user", "captcha");
-        givenValidateUserSuccess(false);
+        givenCorrectValidateLogin("user", "password");
 
-        login("user", "password", "captcha");
+        login(user, pass, "captcha");
 
         // assert
         shouldRedirectTo("/login");
     }
 
-    private void givenValidateUserSuccess(boolean isCorrectLogin) {
-        when(validateUserPort.validate("user", "password")).thenReturn(isCorrectLogin);
+    @Test
+    void login_when_validate_user_fail_then_redirect_to_login() {
+        givenStoredUserAndCaptcha("user", "captcha");
+        givenCorrectValidateLogin("user", "password");
+
+        login("user", "wrong-password", "captcha");
+
+        // assert
+        shouldRedirectTo("/login");
+    }
+
+    private void givenCorrectValidateLogin(String user, String password) {
+        when(validateUserPort.validate(user, password)).thenReturn(true);
     }
 
     @Test
