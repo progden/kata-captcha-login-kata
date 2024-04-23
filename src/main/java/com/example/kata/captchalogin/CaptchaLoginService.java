cb99021160e1862db6a6944df3ab809dade563d4
progden <ccdrumer@gmail.com>
2024-04-23 10:26:31 +0800

    實現登入結果的重定向功能
    在CaptchaLoginService的login方法中，原本返回null的地方現在包含了一個新的登入結果，指向'/home'頁面。LoginResult中添加了一個新的屬性和構造函數，以支持URL的重定向功能。

--- src/main/java/com/example/kata/captchalogin/CaptchaLoginService.java
+++ src/main/java/com/example/kata/captchalogin/CaptchaLoginService.java
@@ -3,6 +3,6 @@ package com.example.kata.captchalogin;
 public class CaptchaLoginService implements CaptchaLoginUseCase {
     @Override
     public LoginResult login(String user, String password, String captcha) {
-        return null;
+        return new LoginResult("/home");
     }
 }
--- src/main/java/com/example/kata/captchalogin/LoginResult.java
+++ src/main/java/com/example/kata/captchalogin/LoginResult.java
@@ -1,7 +1,13 @@
 package com.example.kata.captchalogin;
 
 public class LoginResult {
+    private final String directTo;
+
+    public LoginResult(String redirectUrl) {
+        this.directTo = redirectUrl;
+    }
+
     public String directTo() {
-        return null;
+        return directTo;
     }
 }
