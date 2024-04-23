3b91124bcaf4eb28ed8fdf6cd8e12af21c187234
progden <ccdrumer@gmail.com>
2024-04-23 11:11:13 +0800

    新增使用者輸入驗證於 CaptchaLoginService 登入功能
    在 CaptchaLoginService 的 login 方法中增加了使用者輸入的驗證，包括使用者名稱、密碼與 token 的有效性。假如輸入無效，就會立刻回傳登入失敗。

--- src/main/java/com/example/kata/captchalogin/CaptchaLoginService.java
+++ src/main/java/com/example/kata/captchalogin/CaptchaLoginService.java
@@ -11,11 +11,20 @@ public class CaptchaLoginService implements CaptchaLoginUseCase {
 
     @Override
     public LoginResult login(String inputUser, String inputPass, String inputToken) {
+        // validate input
+        if (inputUser == null || inputUser.isEmpty() || inputUser.isBlank())
+            return new LoginResult("/login");
+        if (inputPass == null || inputPass.isEmpty() || inputPass.isBlank())
+            return new LoginResult("/login");
+        if (inputToken == null || inputToken.isEmpty() || inputToken.isBlank())
+            return new LoginResult("/login");
 
+        // 取得一次性的 toeken 並驗證
         String token = this.captureToken.getToken(inputUser);
         if (!inputToken.equals(token))
             return new LoginResult("/login");
 
+        // 驗證登入
         boolean isValidLogin = this.validateUser.validate(inputUser, inputPass);
         if (!isValidLogin)
             return new LoginResult("/login");
